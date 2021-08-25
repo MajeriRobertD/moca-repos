eebc2c590860967f2e831dcb3ea5f530f93caa0e
AlexandruPosa <alex.posa94@yahoo.com>
2021-08-26 00:40:58 +0300

    userPage styled

--- moca-repos/pages/Users/[UserPage]/index.js
+++ moca-repos/pages/Users/[UserPage]/index.js
@@ -3,12 +3,28 @@ import RenderRepo from "../../../components/renderRepo";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
-function userPage({ user }) {
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    padding: "2%",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    alignItems: "space-between",
+    background:
+      "radial-gradient(circle, rgba(36,9,45,1) 0%, rgba(51,13,13,1) 50% ,rgba(36,9,45,1) 100%)",
+  },
+});
+
+function userPage() {
   const [loading, setLoading] = useState(false);
   const [repos, setRepos] = useState([]);
   const { query } = useRouter();
   const theQuery = query.UserPage;
+  const classes = useStyles();
+
   useEffect(() => {
     try {
       setLoading(true);
@@ -25,17 +41,14 @@ function userPage({ user }) {
   }, [query]);
   return (
     <>
-      <Grid>
-        <div>
-          {repos.map((repo) => (
-            <RenderRepo
-              key={repo.id}
-              repo={repo}
-              user={user}
-              theQuery={theQuery}
-            ></RenderRepo>
-          ))}
-        </div>
+      <Grid className={classes.root}>
+        {repos.map((repo) => (
+          <RenderRepo
+            key={repo.id}
+            repo={repo}
+            theQuery={theQuery}
+          ></RenderRepo>
+        ))}
       </Grid>
     </>
   );
